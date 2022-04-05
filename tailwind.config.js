820d6c32c55c51e6f5d2d0096008fa8101176930
Kane Longbottom <longbottomk@outlook.com>
2022-04-05 21:40:33 +0100

    First

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darktheme: {
+          900: "#121212",
+          800: "#181818",
+        },
+      },
+      fontFamily: {
+        glyphs: ["'Supermercado One', cursive"],
+      },
+    },
   },
   plugins: [],
-}
+};
